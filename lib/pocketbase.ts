ae4e71cea67409ac636b532ce2f4aa81305cbf40
João Davi O. <71538997+Hrafngud@users.noreply.github.com>
2024-11-26 16:34:38 -0300

    Update pocketbase.ts

--- lib/pocketbase.ts
+++ lib/pocketbase.ts
@@ -1,6 +1,7 @@
 import PocketBase from 'pocketbase';
+import 'dotenv/config';
 
-export const pb = new PocketBase('http://127.0.0.1:8090');
+export const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
 
 interface ProjectData {
   title_en: string;
